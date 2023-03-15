19adb62b3855bf8dec5be80e114b4fcb8ff04139
praveen-9920 <114065074+praveen-9920@users.noreply.github.com>
2023-03-15 15:56:04 +0530

    Add files via upload

---
+++ New folder (7)/index.js
@@ -0,0 +1,24 @@
+
+
+// let ar=[1,2,3,4,5,6,7,8,9,10]
+// console.log(ar[2]);
+// let ar1=ar.slice(2,6)
+// console.log(ar1);
+
+
+// let a = []
+// for(i=0;i<=20;i+=2){
+//     a.push(i)
+// }
+// console.log(a);
+
+
+let a =[1,2,3,4]
+let b =0
+for(i=0;i<a.length;i++){
+    if(a[i]>b){
+        b = a[i]
+    }
+}
+console.log(a[i]);
+
