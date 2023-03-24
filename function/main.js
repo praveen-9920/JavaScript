ca4cec15adf24cf3de3cdc7cf5e5d4faf569e400
praveen-9920 <114065074+praveen-9920@users.noreply.github.com>
2023-03-24 16:00:45 +0530

    Add files via upload

---
+++ function/main.js
@@ -0,0 +1,55 @@
+// function myFunction(a){
+//     return("Hi "+a)
+// }
+// console.log(myFunction("Praveen"));
+
+
+// function Name(firstName,lastName,age,salary){
+//     this.firstName = firstName;   
+//     this.lastName = lastName;   
+//     this.age = age;   
+//     this.salary = salary;   
+// }
+// const take = new Name("Praveen","Kumar",19,25000);
+// console.log(take);
+
+
+
+//array - collection of data
+// const praveen = [2,4,6,8,10];
+// console.log(praveen[0]);
+
+
+//obj
+//  const Praveen = {
+//     name:"praveen",//Property
+//     age:19,
+//     f1: function greeting(){
+//         document.write("Good Morning");
+//     } 
+//  }
+
+
+//  console.log(Praveen["f1"]());
+
+
+//   function sum(num1,num2){
+//     console.log(num1+num2);
+//   }
+
+  /*Types of Function
+  1.Functions without arguements and without return value
+  2.Functions without arguements and with return value*/
+
+
+ /*
+ 1.Properties
+ 2.Methods
+ */ 
+
+ function greeting(){
+    console.log("Good Morning");
+    return "Good Morning"
+}
+
+console.log(greeting());
